e32c9dcc313bea69d56a85d130c5b69d717cdac0
sajidgouri <2018pcemesajid705@poornima.org>
2021-10-06 11:20:32 +0530

    model has been created

--- users/models.py
+++ users/models.py
@@ -1,5 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
 
+class member(AbstractBaseUser):
+    Gender_Choices = (
+        ('Male', "Male"),
+        ("female", "Female"),
+    )
 
+    member_name = models.CharField(max_length=20)
+    member_age = models.IntegerField()
+    member_phn_no = models.IntegerField()
+    member_sex = models.CharField(
+        max_length=6,
+        choices= Gender_Choices,
+        default="Male"
+    )
