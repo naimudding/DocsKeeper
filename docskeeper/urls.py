570d3d3ab2ab60e023c1b76d3af610b5ae6dd7aa
Naimuddin <naeemgouri.123@gmail.com>
2021-10-07 11:26:20 +0530

    template linking of image page

--- docskeeper/urls.py
+++ docskeeper/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    #path('', include('main_website.urls')),
+    path('', include('main_website.urls')),
     #path('family/', include('family.urls')),
     #path('docs/', include('documents.urls')),
     #path('accounts/', include('accounts.urls')),
--- main_website/views.py
+++ main_website/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index_view(request):
+    return render(request, 'main_website/index.html')
+
+def feedback_view(request):
+    return render(request, 'main_website/feedback.html')
